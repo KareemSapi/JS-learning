13f9f7447e847c5111b865784846aa2cc51ade31
Kareem Sapi <sapikareem@gmail.com>
2022-07-13 20:53:35 +0300

    Update random.js

--- node/random.js
+++ node/random.js
@@ -3,11 +3,12 @@ function genRandomCode(){
     var num1; 
     var num2;
 
-    var result = Math.floor(num1 + Math.random()*num2);
+    let result = Math.floor(num1 + Math.random()*num2);
 
     result = String(result);
     
-    return typeof result;
+    //return typeof result;
+    return result;
 }
 
 console.log(genRandomCode());
